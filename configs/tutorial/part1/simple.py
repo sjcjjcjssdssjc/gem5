84d162bc61ceeb39d96ba93722ec8b5963be45c3
史佳成 <1339590461@qq.com>
2022-10-06 04:21:37 -0700

    cpu:ok
    Change-Id: Ic3e63cb0fe88847f88202054ca5de6bc736e4503

--- configs/tutorial/part1/simple.py
+++ configs/tutorial/part1/simple.py
@@ -3,11 +3,11 @@ from m5.objects import *
 system = System()
 #father of all
 system.clk_domain = SrcClockDomain()
-system.clk_domain.clk = '1GHz'
+system.clk_domain.clock = '1GHz'
 system.clk_domain.voltage_domain = VoltageDomain()
 
 system.mem_mode = 'timing'
-system.mem_ranges = 'AddrRange('512MB')
+system.mem_ranges = AddrRange('512MB')
 system.cpu = TimingSimpleCPU()
 system.membus = SystemXBar()
 
@@ -18,3 +18,32 @@ system.cpu.dcache_port = system.membus.cpu_side_ports
 #configuration is that, it is allowed to have one port on one side, and
 #an array of ports on the other side
 
+system.cpu.createInterruptController()
+system.cpu.interrupts[0].pio = system.membus.mem_side_ports
+system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
+system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
+system.system_port = system.membus.cpu_side_ports
+#?
+
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.dram = DDR3_1600_8x8()
+#what are those parameters?
+system.mem_ctrl.dram.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.mem_side_ports
+
+binary = 'tests/test-progs/hello/bin/x86/linux/hello'
+system.workload = SEWorkload.init_compatible(binary)
+
+print([binary])
+process = Process()
+process.cmd = [binary]
+system.cpu.workload = process
+system.cpu.createThreads()
+
+root = Root(full_system = False, system = system)
+m5.instantiate()
+
+print("begin sim")
+exit_event = m5.simulate()
+print('Exiting {} tick because of{}'.format(m5.curTick(),
+exit_event.getCause()))
